8988a435f3c3082e4d8924bdf781603eee18b972
BT_3W2_07 <simonh8@nycstudents.net>
2023-03-20 08:45:44 -0400

    a

--- src/DiscussionBoardRunnerV2.java
+++ src/DiscussionBoardRunnerV2.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class DiscussionBoardRunnerV2 {
+import java.util.Scanner;
+
+public class DiscussionBoardRunnerV2 {
+    public static void main(String[] args) {
+        NewsFeed2 nf = new NewsFeed2();
+        //User input to create posts (so the timer has different times!)
+        Scanner s = new Scanner(System.in);
+        //Super basic testing of the classes
+        System.out.print("Msg Test 1 uname-mesg: ");
+        String unameAndMesg = s.nextLine();
+        int dashIndex = unameAndMesg.indexOf("-");
+        String uname = unameAndMesg.substring(0,dashIndex);
+        String msg = unameAndMesg.substring(dashIndex+1);
+        Post mp1 = new MessagePost2(uname, msg);
+        nf.addPost(mp1);
+
+        //Make a second post
+        System.out.print("Msg Test 2 uname-mesg: ");
+        unameAndMesg = s.nextLine();
+        dashIndex = unameAndMesg.indexOf("-");
+        uname = unameAndMesg.substring(0,dashIndex);
+        msg = unameAndMesg.substring(dashIndex+1);
+        Post mp2 = new MessagePost2(uname, msg);
+        nf.addPost(mp2);
+
+        //Make a photo post
+        System.out.print("Photos : uname-filename-caption: ");
+        String unamePhotoCaption = s.nextLine();
+        int dashIndex1 = unamePhotoCaption.indexOf("-");
+        int dashIndex2 = unamePhotoCaption.indexOf("-",dashIndex1+1);
+        uname = unamePhotoCaption.substring(0,dashIndex1);
+        String photoName = unamePhotoCaption.substring(dashIndex1+1, dashIndex2);
+        String caption = unamePhotoCaption.substring(dashIndex2+1);
+        Post pp = new PhotoPost2(uname, photoName, caption);
+        nf.addPost(pp);
+
+        //Print them all
+        System.out.println("++ Test run of the three posts: ++");
+        nf.show();
+
+
+
+
+    }
+
+
 }
